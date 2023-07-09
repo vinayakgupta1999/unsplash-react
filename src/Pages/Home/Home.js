b45673ada3e33b33588a043db42c4e71fb9a1619
vinayak <vinayakalwer2015@gmail.com>
2023-07-09 22:28:40 +0530

    changes

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, {useEffect } from "react";
+import React, { useEffect } from "react";
 import Header from "../../components/Header/Header";
 import Main from "../../components/Main/Main";
 import SplashGrid from "../../components/SplashGrid/SplashGrid";
@@ -6,23 +6,24 @@ import { GetALlImages } from "../../components/api/api";
 import { useDispatch } from "react-redux";
 import { getImages } from "../../redux/ImagesSlice";
 
-
 export default function Home() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     fetchImages();
-  }, [fetchImages]);
+  }, []); 
 
   const fetchImages = async () => {
     try {
       const data = await GetALlImages();
-      console.log(data)
-      dispatch(getImages(data))
+      console.log(data);
+      dispatch(getImages(data));
     } catch (error) {
       console.log("Error fetching images:", error);
     }
   };
+
+
   return (
     <div id="home">
       <Header />
